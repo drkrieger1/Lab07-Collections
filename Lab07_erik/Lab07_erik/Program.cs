11c3fe26a638167efec840479e63d118c3a7e20f
drkrieger <eplyushko@gmail.com>
2017-09-20 16:09:27 -0700

    add and remove added

---
+++ Lab07_erik/Lab07_erik/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab07_erik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my product Collection");
+        }
+    }
+}
